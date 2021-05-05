d29272176d1bf3e90b7a58d13118f5833671f9e6
mwojtulewicz <mateusz.wojtulewicz@gmail.com>
2021-05-05 15:43:40 +0200

    Game.py start

---
+++ Game.py
@@ -0,0 +1,32 @@
+# Game class
+
+import pygetwindow
+import pyautogui
+import numpy as np
+import time
+import mss 
+import os
+
+
+class Game:
+
+    def __init__(self):
+        self.sct = mss.mss()
+    
+    def open_window(self):
+        # starts game window, returns it
+        pass
+
+    def n(self):
+        # no-op
+        pass
+
+    
+
+
+
+
+
+
+
+
