9ad02c55a1feefc3d2564ee89eb1cb131255f887
mwojtulewicz <mateusz.wojtulewicz@gmail.com>
2021-06-13 09:05:14 +0200

    standing exploit note: agent takes 800x380 input shape (it has been later changed) various last layers activations were checked (relu, lrelu...) fitting each timestep (later changed to after episode)

--- dql_agent.py
+++ dql_agent.py
@@ -18,17 +18,32 @@ LR = 0.7
 GAMMA = 0.7
 MAX_EPS = 1
 MIN_EPS = 0.01
-DECAY = 0.2
-NUM_EPISODES = 10
-MAX_TIMESTEPS = 1000
+DECAY = 0.12
+NUM_EPISODES = 100
+MAX_TIMESTEPS = 200
 PENALTY = -1000
+ALPHA = 0.1
+
+hyperparams = {
+    'input_shape': INPUT_SHAPE,
+    'num_actions': NUM_ACTIONS,
+    'learning_rate': LR,
+    'gamma': GAMMA,
+    'min_epsilon': MIN_EPS,
+    'max_epsilon': MAX_EPS,
+    'epsilon_decay': DECAY,
+    'num_episodes': NUM_EPISODES,
+    'max_timesteps': MAX_TIMESTEPS,
+    'loss_penalty': PENALTY,
+    'lrelu_alpha': ALPHA
+}
 
 def model(input_shape=INPUT_SHAPE[1:], output_shape=NUM_ACTIONS):
     model = keras.models.Sequential([
         keras.layers.Flatten(),
         keras.layers.Dense(units=400, activation='relu'),
         keras.layers.Dense(units=200, activation='relu'),
-        keras.layers.Dense(units=output_shape, activation='linear')
+        keras.layers.Dense(units=output_shape, activation=keras.layers.LeakyReLU(alpha=ALPHA))
     ])
     model.compile(optimizer='adam', loss=keras.losses.Huber())
     return model
@@ -36,11 +51,11 @@ def model(input_shape=INPUT_SHAPE[1:], output_shape=NUM_ACTIONS):
 def prepare_observation(obs):
     return obs[:,:,2].reshape(INPUT_SHAPE)
 
-def calculate_reward(reward, done):
-    distance = reward[0]
-    game_steps = reward[1]
+def calculate_reward(score, game_steps, done):
     if not done:
-        return 2*(distance+1)**2 + game_steps/50
+        if score < 0: return 0
+        return score / (game_steps+10)
+        # return 2*(score+1)**2 + game_steps/50  # exploited by standing still
     else:
         return PENALTY
 
@@ -50,37 +65,41 @@ Qnet = model()
 #steps_to_udpate_model = 0
 epsilon = MAX_EPS
 
+timesteps = []
+
 for episode in range(NUM_EPISODES):
 
     print('\n\nEPISODE: {:2d} --> epsilon = {:.2f}'.format(episode,epsilon))
     
-    env.reset()
-    obs,reward,done,info = env.step(0)
+    
+    obs,score,done,info = env.reset()
+    
     obs = prepare_observation(obs)
     qvalues = Qnet(obs).numpy().flatten()
-    R = calculate_reward(reward, done)
+    R = calculate_reward(score, 0, done)
 
     for t in range(MAX_TIMESTEPS):
         #steps_to_udpate_model += 1
-
-        if t%5==0:
-            print(' - timestep: {:<4d}- reward = {:.2f}'.format(t,R))
         
         if np.random.rand() <= epsilon:
             action = np.random.choice(5)
         else:
             action = np.argmax(qvalues)
 
-        n_obs, reward, done, info = env.step(action)
+        n_obs, score, done, info = env.step(action, t%10==0)
         n_obs = prepare_observation(n_obs)
         n_qvalues = Qnet(n_obs).numpy().flatten()
 
-        R = calculate_reward(reward, done)
+        R = calculate_reward(score, t+1, done)
 
         newQ = (1-LR)*qvalues[action] + LR*(R + GAMMA*np.max(n_qvalues))
         target = np.hstack((qvalues[:action],newQ,qvalues[action+1:])).reshape(1,-1)
 
         Qnet.fit(obs, target, verbose=0)
+
+        if t%10==0:
+            print(' - t: {:<4d}- Rt = {:.2f}'.format(t,R))
+            print('   -- qv:', qvalues, '- target:', target[0])
 
         qvalues = n_qvalues
         obs = n_obs
@@ -88,10 +107,19 @@ for episode in range(NUM_EPISODES):
         if done or keyboard.is_pressed('x'):
             break
 
+    timesteps.append(t)
     epsilon = MIN_EPS + (MAX_EPS-MIN_EPS)*np.exp(-DECAY*episode)
     
     if keyboard.is_pressed('x'):
         break
 
 env.close()
-keras.models.save_model(Qnet, 'models/agent1.hdf5')
+keras.models.save_model(Qnet, 'models/agent_dql.hdf5')
+
+plt.figure(figsize=(12,6.5))
+plt.title('Episode durations'); plt.xlabel('Episode number'); plt.ylabel('No timesteps'), plt.grid()
+plt.plot(timesteps)
+plt.savefig('models/fig')
+plt.show()
+
+# np.save('models/timesteps',timesteps)
